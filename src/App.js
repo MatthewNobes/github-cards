904ee51af31073ebe77bed313421aa9c13bb2333
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-09-29 13:59:00 +0100

    converting components to class ones

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-const CardList = (props) => (
-  /**This takes an array of objects (people and their data) and maps out the data into a new 
-   * array called profile. This is then used to fill Card components with the profile data
-   * until every item in the array has had a component generated for it.
-   */
-  <div>
-    {props.profiles.map(profile => <Card {...profile}/>)}
-  </div>
-)
+
+class CardList extends React.Component{
+  
+
+  render() {
+
+    return(
+      /**This takes an array of objects (people and their data) and maps out the data into a new 
+     * array called profile. This is then used to fill Card components with the profile data
+     * until every item in the array has had a component generated for it.
+     */
+      <div>
+        {this.props.profiles.map(profile => <Card {...profile}/>)}
+      </div>
+    )
+  }
+}
 
 class Card extends React.Component{
   render() {
-
     const profile = this.props;
-
     return (
       <div className="github-profile">
           <img src={profile.avatar_url} />
